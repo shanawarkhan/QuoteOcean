abd4f4246920156383ffcecea072979a3ad622f7
Shanawarkhan <shanawar_k@hotmail.com>
2015-10-03 07:12:18 +0500

    adding all the files except modules

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	"database": "mongodb://admin:modulus9306@apollo.modulusmongo.net:27017/Amevo6ta",
+	"port": process.env.PORT || 4000,
+	"secretKey": "YourSecretKey"
+
+}
+
+
+// "database": "mongodb://admin:admin9306@apollo.modulusmongo.net:27017/Amevo6ta"
